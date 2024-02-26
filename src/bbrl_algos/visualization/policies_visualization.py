c4e6623b0580efde4d449c28499ccbd3439933f4
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2024-02-26 13:14:16 +0100

    Update policies_visualization.py

--- src/bbrl_algos/visualization/policies_visualization.py
+++ src/bbrl_algos/visualization/policies_visualization.py
@@ -62,8 +62,8 @@ def get_best_3_policies(cfg_raw,logger):
 def main(cfg_raw: DictConfig):
     #print(read_tensor_file())
     logger = Logger(cfg_raw)
-    get_best_3_policies = get_best_3_policies(cfg_raw,logger)
-    return(get_best_3_policies)
+    policies = get_best_3_policies(cfg_raw,logger)
+    print(policies)
 
 if __name__ == "__main__":
     main()
