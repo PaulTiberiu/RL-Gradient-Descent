c1d4f32f4595d3c76e59849677bb7faf7ca734c8
Iordache Paul-Tiberiu <iordache.tiberiu19@yahoo.com>
2024-03-17 14:01:04 +0100

    Solving alphas with a linear program, plotting how it is supposed to

--- src/bbrl_algos/visualization/policies_visualization.py
+++ src/bbrl_algos/visualization/policies_visualization.py
@@ -22,6 +22,8 @@ from bbrl_algos.algos.dqn.dqn import local_get_env_agents
 from bbrl_algos.models.critics import DiscreteQAgent
 from bbrl.workspace import Workspace
 
+import plotly.graph_objects as go
+
 
 
 def extract_number_from_filename(filename):
@@ -129,7 +131,7 @@ def plot_triangle_with_multiple_points(coefficients_list, rewards_list):
     """
 
     # Generate the vertices of the equilateral triangle
-    triangle_vertices = np.array([[0, 0], [1, 0], [0.5, np.sqrt(3)/2], [0, 0]])
+    triangle_vertices = np.array([[0, 0], [1, 0], [0.5, np.sqrt(3)/2],[0,0]])
 
     # Plot the triangle edges
     plt.plot(triangle_vertices[:, 0], triangle_vertices[:, 1], 'k-')
@@ -144,19 +146,20 @@ def plot_triangle_with_multiple_points(coefficients_list, rewards_list):
 
     # Choose a colormap that covers the entire range of rewards
     cmap = plt.get_cmap('RdBu')
+    #cmap = plt.get_cmap('viridis')
 
     # Plot the points with adjusted positions and transparency
     for coefs, reward in zip(coefficients_list, rewards_list):
         new_point = np.dot(np.array(coefs), triangle_vertices[:3])
-        jitter = np.random.normal(0, 0.01, 2)  # Add a small random jitter to avoid superposition
-        plt.scatter(new_point[0] + jitter[0], new_point[1] + jitter[1], c=reward, cmap=cmap, norm=norm, alpha=0.5, s=250)
-
+        #jitter = np.random.normal(0, 0.01, 2)  # Add a small random jitter to avoid superposition
+        #plt.scatter(new_point[0] + jitter[0], new_point[1] + jitter[1], c=reward, cmap=cmap, norm=norm, alpha=0.5, s=250)
+        plt.scatter(new_point[0], new_point[1], c=reward, cmap=cmap, norm=norm, s=100)
     # Set axis limits and labels
     plt.xlim(-0.1, 1.1)
     plt.ylim(-0.1, np.sqrt(3)/2 + 0.1)
 
     # Add color bar legend
-    cbar = plt.colorbar(cm.ScalarMappable(cmap=cmap, norm=norm))
+    cbar = plt.colorbar(cm.ScalarMappable(cmap=cmap, norm=norm), ax=plt.gca())
     cbar.set_label('Reward')
 
     # Add legend
@@ -180,6 +183,89 @@ def plot_triangle_with_multiple_points(coefficients_list, rewards_list):
 
     # Show the plot
     plt.show()
+
+
+
+def plot_triangle_with_multiple_points_plotly(coefficients_list, rewards_list):
+    """
+    Plot a triangle with vertices representing policies and a new point calculated as a weighted sum of policies.
+    Color the new point based on its reward value.
+
+    Parameters:
+    - coefficients_list (list of lists): List of coefficients (a1, a2, a3) used to calculate the new point.
+    - rewards_list (list of torch.Tensor): List of reward tensors for each point.
+    """
+
+    # Convert Torch tensors to standard Python numbers
+    rewards_list = [reward.item() for reward in rewards_list]
+
+    # Generate the vertices of the equilateral triangle
+    triangle_vertices = np.array([[0, 0], [1, 0], [0.5, np.sqrt(3)/2],[0,0]])
+
+    # Create trace for triangle edges
+    triangle_edges = go.Scatter(
+        x=triangle_vertices[:, 0],
+        y=triangle_vertices[:, 1],
+        mode='lines',
+        line=dict(color='black'),
+        showlegend=False,  # Set this to False to hide from legend
+    )
+
+    # Create trace for policy vertices
+    policy_vertices = go.Scatter(
+        x=[triangle_vertices[0, 0], triangle_vertices[1, 0], triangle_vertices[2, 0]],
+        y=[triangle_vertices[0, 1], triangle_vertices[1, 1], triangle_vertices[2, 1]],
+        mode='text',
+        text=['P1', 'P2', 'P3'],
+        textposition='top center',
+        textfont=dict(size=20),
+        showlegend=False,  # Set this to False to hide from legend
+    )
+
+    # Create trace for new points
+    min_reward = min(rewards_list)
+    max_reward = max(rewards_list)
+
+
+    # Create trace for new points with the updated colorbar attributes
+    new_points = []
+    for coefs, reward in zip(coefficients_list, rewards_list):
+        new_point = np.dot(np.array(coefs), triangle_vertices[:3])
+        hover_text = f"Reward: {reward:.2f}<br>Coordinates: ({new_point[0]:.2f}, {new_point[1]:.2f})<br>Alphas: {coefs}"
+        new_points.append(go.Scatter(
+            x=[float(new_point[0])],
+            y=[float(new_point[1])],
+            mode='markers',
+            marker=dict(
+                color=[reward],  # Enclose reward in a list to define its color based on the 'RdBu' scale
+                size=10,
+                colorbar=dict(
+                    title='Reward',
+                    tickvals=[min_reward, max_reward],
+                    ticktext=[f'{min_reward:.2f}', f'{max_reward:.2f}']
+                ),
+                colorscale='RdBu',  # Use the built-in Red-Blue color scale
+                cmin=min_reward,  # Explicitly set the min for color scaling
+                cmax=max_reward,  # Explicitly set the max for color scaling
+                showscale=True  # Ensure that the colorscale is shown
+            ),
+            text=[hover_text],
+            hoverinfo='text',
+            showlegend=False,
+        ))
+
+
+    # Create layout
+    layout = go.Layout(
+        title='Triangle Plot',
+        xaxis=dict(title='X-axis', range=[-0.1, 1.1]),
+        yaxis=dict(title='Y-axis', range=[-0.1, np.sqrt(3)/2 + 0.1]),
+        showlegend=True
+    )
+
+    # Plot
+    fig = go.Figure(data=[triangle_edges, policy_vertices] + new_points, layout=layout)
+    fig.show()
 
 
 def update_policy_with_coefficients(list_policies, coefficients):
@@ -223,29 +309,6 @@ def get_policy_reward(coefficients, loaded_agents):
 
     return new_policy_reward
 
-
-def generate_coefficients_left_bound_triangle(num_points):
-
-    # Generate random barycentric coordinates for each point
-    r1 = np.random.uniform(0, 1, num_points)
-    r2 = np.random.uniform(0, 1, num_points)
-    is_inside_triangle = r1 + r2 <= 1
-
-    # Keep generating until all points are inside the triangle
-    while not np.all(is_inside_triangle):
-        r1[~is_inside_triangle] = np.random.uniform(0, 1, np.sum(~is_inside_triangle))
-        r2[~is_inside_triangle] = np.random.uniform(0, 1, np.sum(~is_inside_triangle))
-        is_inside_triangle = r1 + r2 <= 1
-
-    r1 = r1[is_inside_triangle]
-    r2 = r2[is_inside_triangle]
-    r3 = 1 - r1 - r2
-
-    # Combine barycentric coordinates into coefficients
-    coefficients_list = np.column_stack((r1, r2, r3)).tolist()
-
-    return coefficients_list
-
 def generate_coefficients_list(num_points):
 
     # Generate random coefficients for each point
@@ -273,6 +336,146 @@ def save_policy_and_get_agent(policy):
 
     return new_agent
 
+def intersection_point(m, b, x_coord):
+    "Calculates the intersection point of an y = mx'+b axis with the x = x_coord axis"
+    # Calculate y-coordinate of intersection point
+    y = m * (x_coord) + b
+    return x_coord, y
+
+def find_axis_through_point(point, slope):
+    # Point coordinates
+    x_h, y_h = point
+    
+    # Equation of the axis passing through H: y = mx + b
+    # We know that the slope is the same as the given slope
+    # So, b = y - mx
+    b = y_h - slope * x_h
+    
+    # Return the equation of the axis: y = slope * x + b
+    return slope, b
+
+def generate_left_edge_points(num_points):
+    # Generate x values linearly spaced between 0 and 1/2
+    x_values = np.linspace(0, 0.5, num_points)
+    
+    # Calculate corresponding y values based on the equation sqrt(3) * x
+    y_values = np.sqrt(3) * x_values
+    
+    # Combine x and y values into a list of points
+    points = list(zip(x_values, y_values))
+    
+    return points
+
+def generate_lower_edge_points(num_points):
+    # Generate x values linearly spaced between 0 and 1
+    x_values = np.linspace(0, 1, num_points)
+    
+    # Set y values to zero for all points
+    y_values = np.zeros_like(x_values)
+    
+    # Combine x and y values into a list of points
+    points = list(zip(x_values, y_values))
+    
+    return points
+
+def get_alphas_from_point(x,y):
+    triangle_vertices = [[0, 0],[1, 0], [0.5, np.sqrt(3)/2]]
+    #print("triangle_vertices : ", triangle_vertices)
+    A = np.vstack([triangle_vertices[0], triangle_vertices[1], triangle_vertices[2]]).T
+    #print("A : ", A)
+
+    A = np.vstack([A, np.ones(3)])
+
+    b = np.array([x, y, 1])  
+
+    # Résolution du système d'équations
+    alphas = np.linalg.lstsq(A, b, rcond=None)[0]
+
+    # Normalisation pour s'assurer que les alphas sont positifs ou nuls et somment à 1
+    alphas = np.maximum(alphas, 0)  # Les valeurs négatives deviennent 0
+    alphas /= np.sum(alphas)  # Normalisation pour que la somme soit égale à 1
+
+    #print("alphas: ",alphas)
+
+    return alphas
+
+def is_inside_triangle(point, A, B, C):
+
+    def sign(p1, p2, p3):
+        return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
+
+    b1 = sign(point, A, B) < 0.0
+    b2 = sign(point, B, C) < 0.0
+    b3 = sign(point, C, A) < 0.0
+
+    return ((b1 == b2) and (b2 == b3))
+
+def policies_visualization(num_points, loaded_agents):
+
+    #1. Calculating alphas
+
+    points = generate_left_edge_points(num_points)
+    points2 = generate_lower_edge_points(num_points)
+
+    axis_equations_points = []
+    i=0
+    slope = -np.sqrt(3)/3
+    for point in points:
+        new_slope, b = find_axis_through_point(point,slope)
+        axis_equations_points.append((new_slope, b))
+
+    intersection = []
+    i = 0
+    j = 0
+    for i in range(len(axis_equations_points)):
+        for j in range(len(points2)):
+            slope, b = axis_equations_points[i]
+            x_coord = points2[j][0]
+            intersection.append(intersection_point(slope, b, x_coord))
+
+    #print(intersection)
+
+    # Extract x and y coordinates from points
+    x_points = [point[0] for point in points]
+    y_points = [point[1] for point in points]
+
+    # Extract x and y coordinates from points2
+    x_points2 = [point[0] for point in points2]
+    y_points2 = [point[1] for point in points2]
+
+    # Extract x and y coordinates from intersection
+    x_intersection = [point[0] for point in intersection]
+    y_intersection = [point[1] for point in intersection]
+
+    alphas = []
+    i = 0
+    for i in range(len(x_points)):
+        if(is_inside_triangle([x_points[i],y_points[i]], [0, 0], [1, 0], [0.5, np.sqrt(3)/2])):
+            alphas.append(get_alphas_from_point(x_points[i],y_points[i]))
+    j = 0
+    for j in range(len(x_points2)):
+        if(is_inside_triangle([x_points2[j],y_points2[j]], [0, 0], [1, 0], [0.5, np.sqrt(3)/2])):
+            alphas.append(get_alphas_from_point(x_points2[j],y_points2[j]))
+    k = 0
+    for k in range(len(x_intersection)):
+        if(is_inside_triangle([x_intersection[k],y_intersection[k]], [0, 0], [1, 0], [0.5, np.sqrt(3)/2])):
+            alphas.append(get_alphas_from_point(x_intersection[k],y_intersection[k]))
+
+    #print(len(alphas))
+
+    #2. Calculating rewards
+    rewards_list = []
+    cpt=0
+    for alpha in alphas:
+        rewards_list.append(get_policy_reward(alpha, loaded_agents))
+        cpt+=1
+        print(cpt)
+
+    #3. Plotting the triangle with the points
+    plot_triangle_with_multiple_points_plotly(alphas, rewards_list)
+    plot_triangle_with_multiple_points(alphas, rewards_list)
+    
+
 
 @hydra.main(
     config_path="../algos/dqn/configs/",
@@ -297,30 +500,7 @@ def main(cfg_raw: DictConfig):
 
     loaded_agents = load_best_agent()
     #print(loaded_agents)
-
-    list_policies = load_policies(loaded_agents)
-
-
-    coefficients = [0, 0, 1]
-    new_policy_reward = get_policy_reward(coefficients, loaded_agents)
-    print(new_policy_reward)
-
-
-    coefficients_list = generate_coefficients_list(400)
-    rewards_list = []
-    cpt=0
-    for coeff in coefficients_list:
-        rewards_list.append(get_policy_reward(coeff, loaded_agents))
-        cpt+=1
-        print(cpt)
-
-    plot_triangle_with_multiple_points(coefficients_list, rewards_list)
-
-
-    #new_policy = update_policy_with_coefficients(list_policies, coefficients)
-    #print(new_policy)
-    # new_agent = save_policy_and_get_agent(new_policy)
-    # print(new_agent)
+    policies_visualization(40, loaded_agents)
 
 
 
